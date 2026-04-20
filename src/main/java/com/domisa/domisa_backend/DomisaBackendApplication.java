c7a5af77e0fc5ec0f961ac032a3d5e3db712929b
chohaewon48 <chocynthia@naver.com>
2026-04-20 18:35:35 +0900

    feat: 초기 프로젝트 세팅

---
+++ src/main/java/com/domisa/domisa_backend/DomisaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.domisa.domisa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomisaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomisaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domisa/domisa_backend/DomisaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domisa.domisa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomisaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
