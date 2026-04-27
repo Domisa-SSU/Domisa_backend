a576f31d85ae6c70a7f82a5075497d59833ac2d7
youngroky <youngrok886@gmail.com>
2026-04-27 16:07:38 +0900

    fix: ENUM 종류들

--- src/main/java/com/domisa/domisa_backend/user/type/AnimalProfile.java
+++ src/main/java/com/domisa/domisa_backend/user/type/AnimalProfile.java
@@ -1,12 +1,16 @@
 package com.domisa.domisa_backend.user.type;
 
 public enum AnimalProfile {
-	DOG,
-	CAT,
-	FOX,
-	BEAR,
-	RABBIT,
-	DEER,
-	WOLF,
-	TIGER
+	DOG, // 강아지
+	CAT, // 고양이
+	BEAR, // 곰
+	SLOTH, // 나무늘보
+	HAMSTER, // 햄스터
+	WOLF, // 늑대
+	RABBIT, // 토끼
+	DEER, // 사슴
+	OTTER, // 수달
+	ALPACA, // 알파카
+	FOX, // 여우
+	CAPYBARA // 카피바라
 }
