7e72eea3dadc168149f6cc8bf6f89d67d60b28de
youngroky <youngrok886@gmail.com>
2026-04-28 16:23:24 +0900

    fix: DTO 내용 수정

--- src/main/java/com/domisa/domisa_backend/introduction/dto/IntroductionResponse.java
+++ src/main/java/com/domisa/domisa_backend/introduction/dto/IntroductionResponse.java
@@ -2,28 +2,21 @@ package com.domisa.domisa_backend.introduction.dto;
 
 import com.domisa.domisa_backend.introduction.entity.Introduction;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
 
 public record IntroductionResponse(
-	@JsonProperty("introduction_id") Long introductionId,
-	List<IntroductionItem> introductions
+	@JsonProperty("introduction_id")
+	Long introductionId,
+	String q1,
+	String q2,
+	String q3
 ) {
 
 	public static IntroductionResponse from(Introduction introduction) {
 		return new IntroductionResponse(
 			introduction.getId(),
-			List.of(new IntroductionItem(
-				introduction.getQ1(),
-				introduction.getQ2(),
-				introduction.getQ3()
-			))
+			introduction.getQ1(),
+			introduction.getQ2(),
+			introduction.getQ3()
 		);
-	}
-
-	public record IntroductionItem(
-		String q1,
-		String q2,
-		String q3
-	) {
 	}
 }
