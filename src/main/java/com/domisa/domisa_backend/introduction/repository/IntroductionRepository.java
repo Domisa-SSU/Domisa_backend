f0db845402757aa8000cae409a6f3dd2a9e43ea1
youngroky <youngrok886@gmail.com>
2026-04-28 16:42:57 +0900

    delete: no usage method

--- src/main/java/com/domisa/domisa_backend/introduction/repository/IntroductionRepository.java
+++ src/main/java/com/domisa/domisa_backend/introduction/repository/IntroductionRepository.java
@@ -5,8 +5,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IntroductionRepository extends JpaRepository<Introduction, Long> {
-
-	Optional<Introduction> findByParticipantId(Long participantId);
-
 	Optional<Introduction> findByLinkCode(String linkCode);
 }
