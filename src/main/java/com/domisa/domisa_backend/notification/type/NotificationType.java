14735b0dd1b97f7a681b9b7faa6306d74553c0c7
youngroky <youngrok886@gmail.com>
2026-04-30 22:36:06 +0900

    fix: ENUM

--- src/main/java/com/domisa/domisa_backend/notification/type/NotificationType.java
+++ src/main/java/com/domisa/domisa_backend/notification/type/NotificationType.java
@@ -1,7 +1,7 @@
 package com.domisa.domisa_backend.notification.type;
 
 public enum NotificationType {
-	LIKE,
-	INTRODUCTION,
-	SHUFFLE
+	LIKE, // 호감표시를 받음
+	INTRODUCTION, // 소개서 도착
+	SHUFFLE // 새로고침 가능
 }
