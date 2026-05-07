d6de8bb7ad4c941c10430d20292db8a116e5dbf8
youngroky <youngrok886@gmail.com>
2026-05-07 17:36:56 +0900

    feat: 소개팅 카드 자동 갱신 정책 추가

--- src/main/java/com/domisa/domisa_backend/dating/dto/DatingRefreshTimeResponse.java
+++ src/main/java/com/domisa/domisa_backend/dating/dto/DatingRefreshTimeResponse.java
@@ -3,7 +3,6 @@ package com.domisa.domisa_backend.dating.dto;
 import java.time.LocalDateTime;
 
 public record DatingRefreshTimeResponse(
-	LocalDateTime refreshAvailableAt,
-	boolean canRefresh
+	LocalDateTime refreshAvailableAt
 ) {
 }
